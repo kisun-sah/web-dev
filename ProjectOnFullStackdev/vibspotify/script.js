10eb50923556def3f73c9152b28416013cfd5269
kisun-sah <kishunsah783@gmail.com>
2024-07-19 00:26:28 +0530

    add update 002

--- ProjectOnFullStackdev/vibspotify/script.js
+++ ProjectOnFullStackdev/vibspotify/script.js
@@ -1,5 +1,22 @@
 console.log('javascript');
 let currentsong = new Audio();
+let  songs;
+
+
+
+function secondsToMinutesSeconds(seconds) {
+    if (isNaN(seconds) || seconds < 0) {
+        return "00:00";
+    }
+
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = Math.floor(seconds % 60);
+
+    const formattedMinutes = String(minutes).padStart(2, '0');
+    const formattedSeconds = String(remainingSeconds).padStart(2, '0');
+
+    return `${formattedMinutes}:${formattedSeconds}`;
+}
 
 async function getsongs() {
     try {
@@ -24,17 +41,20 @@ async function getsongs() {
     }
 }
 
-const playMusic = (track) => {
+const playMusic = (track , pause = false) => {
     currentsong.src = "/songs/" + track; // Corrected property name
-    currentsong.play();
-    play.src = "pause.svg"
-    document.querySelector(".songinfo").innerHTML = track
+    if(!pause){
+        currentsong.play();
+            play.src = "pause.svg"
+    }
+    document.querySelector(".songinfo").innerHTML = decodeURI(track);
     document.querySelector(".songtime").innerHTML = "00:00 / 00:00"
 }
 
 async function main() {
     // get the list of all the songs
-    let songs = await getsongs();
+     songs = await getsongs();
+    playMusic(songs[0] ,true);
 
     // show all the songs in the playlist 
     let songUl = document.querySelector(".songlist").getElementsByTagName("ul")[0];
@@ -72,6 +92,52 @@ async function main() {
              play.src = "play-circle.svg"
         }
     })
+    // Listen for timeupdate event
+    currentsong.addEventListener("timeupdate", () => {
+        document.querySelector(".songtime").innerHTML = `${secondsToMinutesSeconds( currentsong.currentTime)} / ${secondsToMinutesSeconds(currentsong.duration)}`
+        document.querySelector(".circle").style.left = ( currentsong.currentTime  /  currentsong.duration) * 100 + "%";
+    })
+
+    //Add  an event listener seekbar
+    document.querySelector('.seekbar').addEventListener('click' , (e) => {
+     let percent = ( e.offsetX /e.target.getBoundingClientRect().width )* 100;
+    document.querySelector('.circle').style.left=percent +"%";
+    currentsong.currentTime = ((currentsong.duration)*percent)/100
+    }) 
+
+    // Add an event listener for hamburgerIcon
+    document.querySelector('.hamburger').addEventListener(('click' ), () =>{
+    document.querySelector('.left').style.left = '0'
+    })
+
+      // Add an event listener for close button 
+      document.querySelector('.close').addEventListener(('click') , () =>{
+        document.querySelector('.left').style.left = '-120%'
+      })
+
+      //Add and event listener to previous  
+      previous.addEventListener('click' , () =>{
+        let index = songs.indexOf(   currentsong.src.split("/").slice(-1)[0]);
+        if((index-1) >= 0){
+
+            playMusic(songs[index-1])
+        }
+      })
+        //Add and event listener to  Next.
+      next.addEventListener('click' , () =>{
+        currentsong.pause();
+
+        let index = songs.indexOf(   currentsong.src.split("/").slice(-1)[0]);
+        if((index+1) < songs.length){
+
+            playMusic(songs[index+1])
+        }
+      })
+
+      // Add an event to volume 
+      document.querySelector('.range').getElementsByTagName('input')[0].addEventListener(("change"),(e) => {
+        currentsong.volume = parseInt(e.target.value)/100;
+      })
 }
 
 main();
